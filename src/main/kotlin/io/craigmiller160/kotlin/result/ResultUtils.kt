59809698e50ff9b90b50dec277c98952e8278e7c
Craig Miller <craigmiller160@gmail.com>
2021-07-25 17:24:25 -0400

    Added ResultUtils

---
+++ src/main/kotlin/io/craigmiller160/kotlin/result/ResultUtils.kt
@@ -0,0 +1,31 @@
+package io.craigmiller160.kotlin.result
+
+fun <T,R> Result<T>.flatMap(transform: (T) -> Result<R>): Result<R> {
+    if (isSuccess) {
+        return transform(getOrThrow())
+    }
+    return this as Result<R>
+}
+
+fun <T> Result<T>.recoverAndFlatten(transform: (Throwable) -> Result<T>): Result<T> {
+    if (isSuccess) {
+        return this
+    }
+    return transform(exceptionOrNull()!!)
+}
+
+fun <T> Result<T>.recoverCatchingAndFlatten(transform: (Throwable) -> Result<T>): Result<T> {
+    if (isSuccess) {
+        return this
+    }
+    return runCatching {
+        transform(exceptionOrNull()!!)
+    }.flatten()
+}
+
+fun <T> Result<Result<T>>.flatten(): Result<T> {
+    if (isSuccess) {
+        return getOrThrow()
+    }
+    return Result.failure(exceptionOrNull()!!)
+}
