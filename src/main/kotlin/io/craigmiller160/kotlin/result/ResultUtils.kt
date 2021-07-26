4a30d87a651d6195e2a1c41e3ba1162a3bbd3d1e
Craig Miller <craigmiller160@gmail.com>
2021-07-26 14:51:40 -0400

    Refactored the implementation of the functions

--- src/main/kotlin/io/craigmiller160/kotlin/result/ResultUtils.kt
+++ src/main/kotlin/io/craigmiller160/kotlin/result/ResultUtils.kt
@@ -1,31 +1,27 @@
 package io.craigmiller160.kotlin.result
 
-fun <T,R> Result<T>.flatMap(transform: (T) -> Result<R>): Result<R> {
-    if (isSuccess) {
-        return transform(getOrThrow())
-    }
-    return this as Result<R>
-}
+fun <T,R> Result<T>.flatMap(transform: (T) -> Result<R>): Result<R> =
+        when {
+            isSuccess -> transform(getOrThrow())
+            else -> this as Result<R>
+        }
 
-fun <T> Result<T>.flatRecover(transform: (Throwable) -> Result<T>): Result<T> {
-    if (isSuccess) {
-        return this
-    }
-    return transform(exceptionOrNull()!!)
-}
+fun <T> Result<T>.flatRecover(transform: (Throwable) -> Result<T>): Result<T> =
+        when {
+            isSuccess -> this
+            else -> transform(exceptionOrNull()!!)
+        }
 
-fun <T> Result<T>.flatRecoverCatching(transform: (Throwable) -> Result<T>): Result<T> {
-    if (isSuccess) {
-        return this
-    }
-    return runCatching {
-        transform(exceptionOrNull()!!)
-    }.flatten()
-}
+fun <T> Result<T>.flatRecoverCatching(transform: (Throwable) -> Result<T>): Result<T> =
+        when {
+            isSuccess -> this
+            else -> runCatching {
+                transform(exceptionOrNull()!!)
+            }.flatten()
+        }
 
-fun <T> Result<Result<T>>.flatten(): Result<T> {
-    if (isSuccess) {
-        return getOrThrow()
-    }
-    return Result.failure(exceptionOrNull()!!)
-}
+fun <T> Result<Result<T>>.flatten(): Result<T> =
+        when {
+            isSuccess -> getOrThrow()
+            else -> Result.failure(exceptionOrNull()!!)
+        }
